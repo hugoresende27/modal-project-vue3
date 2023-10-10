5fb3be61f1bcb684dd830060178408c2c0143203
hugoresende <hugo.resende@iservices.pt>
2023-10-10 21:26:53 +0100

    pass data (props) between components

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,58 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <h1>My Vue App</h1>
-  <p>{{ title }}</p>
+  <p>{{ title }}</p>  
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+
+  <Modal :header="header" :text="text" theme="sale"/>
+
+
   
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  data(){
-    return {
-      title: 'Hugão!'
-    }
-  },
   components: {
     // HelloWorld
+    Modal
   },
+  data(){
+    return {
+      title: 'Hugão!',
+      header: 'Sign up for the Giveway!!!',
+      text: 'Grab your ninja swag for half price now !'
+    }
+  },
+
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1 {
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
+  }
 </style>
